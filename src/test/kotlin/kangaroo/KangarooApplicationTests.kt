1c1f9af5c88c916cc9ce385b9e2c7b452f6a121e
sddtc <changhbaga@gmail.com>
2022-11-09 17:40:25 +0800

    [k-0][k-a] initial kangaroo
    Co-Authored By: hui-chang-tw <hchang@thoughtworks.com>

---
+++ src/test/kotlin/kangaroo/KangarooApplicationTests.kt
@@ -0,0 +1,13 @@
+package kangaroo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KangarooApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
